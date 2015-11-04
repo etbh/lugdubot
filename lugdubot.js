1658f8548a0d7f034dd883f77c9e25ce266079a0
Chimyx <chimyx@live.fr>
2015-11-04 22:49:59 +0100

    Reduced text size

--- lugdubot.js
+++ lugdubot.js
@@ -21,6 +21,6 @@ window.setInterval(function(){
         xhr.open('POST','http://www.camarilla-fr.com/forum/ajaxshoutbox/post', false);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-        xhr.send("text_shoutbox=%5Bsize%3D150%5D%5Bb%5D%5Bcolor%3D%23FF0000%5D%5Bhighlight%3Dyellow%5DLUGDUBOT+IS+ALIVE%5B%2Fhighlight%5D%5B%2Fcolor%5D%5B%2Fb%5D%5B%2Fsize%5D&creation_time="+document.querySelector('[name=creation_time]').value+"&form_token=" + document.querySelector('[name=form_token]').value);
+        xhr.send("text_shoutbox=%5Bsize%3D50%5D%5Bb%5D%5Bcolor%3D%23FF0000%5D%5Bhighlight%3Dyellow%5DLUGDUBOT+IS+ALIVE%5B%2Fhighlight%5D%5B%2Fcolor%5D%5B%2Fb%5D%5B%2Fsize%5D&creation_time="+document.querySelector('[name=creation_time]').value+"&form_token=" + document.querySelector('[name=form_token]').value);
     }
 }, 2000);
