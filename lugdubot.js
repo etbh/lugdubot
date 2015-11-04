8c0b28f423fac5374420b9f5ceaed88ccab1c318
Chimyx <chimyx@live.fr>
2015-11-04 23:00:07 +0100

    Removed useless things and added log

--- lugdubot.js
+++ lugdubot.js
@@ -11,20 +11,21 @@ function getNewMessages(){
         messages = messages.slice(-1);
     if (messages.length > 0)
         lastFetched = messages[messages.length - 1].id;
-    console.log(lastFetched);
     return messages;
 }
 
 
 window.setInterval(function(){
-    var invocation = false;
     if (getNewMessages().some(function(entry){
         return entry.message == "lugdubot";
     })){
-        xhr = new XMLHttpRequest();
+        console.log('HELLO');
+        var xhr = new XMLHttpRequest();
         xhr.open('POST','http://www.camarilla-fr.com/forum/ajaxshoutbox/post', false);
         xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         xhr.send("text_shoutbox=%5Bsize%3D50%5D%5Bb%5D%5Bcolor%3D%23FF0000%5D%5Bhighlight%3Dyellow%5DLUGDUBOT+IS+ALIVE%5B%2Fhighlight%5D%5B%2Fcolor%5D%5B%2Fb%5D%5B%2Fsize%5D&creation_time="+document.querySelector('[name=creation_time]').value+"&form_token=" + document.querySelector('[name=form_token]').value);
     }
+    else
+        console.log('GOT NOTHING...');
 }, 2000);
